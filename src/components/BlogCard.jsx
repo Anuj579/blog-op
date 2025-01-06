3d8ee06d61d67c6a35625d11fad2675bd1b5cfd9
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-01-06 13:09:31 +0530

    updated blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -9,19 +9,21 @@ export function BlogCard({ post }) {
 
     return (
         <Card className="flex flex-col overflow-hidden h-full hover:shadow-lg transition-shadow duration-300">
-            <Image
-                src={post.coverImage || "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-                alt={post.title}
-                width={300}
-                height={200}
-                className="w-full h-52 object-cover hover:scale-105 transition-transform duration-300"
-                priority={true}
-            />
+            <div className="h-52 overflow-hidden">
+                <Image
+                    src={post.coverImage || "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                    alt={post.title}
+                    width={300}
+                    height={200}
+                    className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+                    priority={true}
+                />
+            </div>
             <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
             </CardHeader>
             <CardContent className="flex-grow">
-                <p className="text-muted-foreground line-clamp-2 mb-2">{post.content}</p>
+                <p className="text-muted-foreground line-clamp-2 mb-2" dangerouslySetInnerHTML={{ __html: post.content }} />
             </CardContent>
             <CardFooter className='flex flex-col items-start gap-2'>
                 <p className="text-sm text-muted-foreground">By {post.author.firstname + " " + post.author.lastname} - {formatDate(post.createdAt)}</p>
